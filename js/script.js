fda04c895f449ab89e99151ad529162365dea29f
Anna <ackongs@gmail.com>
2022-06-22 18:57:41 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const createVehicle = function (type, numWheels, color) {
+  const vehicle = {};
+  vehicle.type = type;
+  vehicle.numWheels = numWheels;
+  vehicle.color = color;
+
+  return vehicle;
+};
+
+const car = createVehicle("car", 2, "pink");
+const bike = createVehicle("bike", 1, "yellow");
+const skateboard = createVehicle("skateboard", 4, "green");
+
+const myVehicles = [car, bike, skateboard];
+
+for (let vehicle of myVehicles) {
+  for (let key in vehicle) {
+    console.log(key, vehicle[key]);
+  }
+}
